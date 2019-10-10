0ce92bf6915438782df0588edbedaa9dcc073d45
yonlu <lss.emailbox@gmail.com>
2019-10-10 11:44:40 -0400

    feat: base css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import './App.css';
 
+import logo from './assets/logo.svg';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <img src={logo} alt="AirCnC logo"/>
+
+      <div className="content">
+        <p>Offer <strong>spots</strong> for programmers and find new <strong>talent</strong> for your company!</p>
+
+        <form>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" placeholder="Email" />
+        </form>
+      </div>
     </div>
   );
 }
