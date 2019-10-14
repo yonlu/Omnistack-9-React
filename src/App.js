5fb2965660388e78af6fd6213b4661e33c90ac47
yonlu <lss.emailbox@gmail.com>
2019-10-14 11:30:58 -0400

    chore: eslint, front page css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from './services/api';
 import './App.css';
 
 import logo from './assets/logo.svg';
 
 function App() {
+  const [email, setEmail] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    // email
+  }
+
   return (
     <div className="container">
-      <img src={logo} alt="AirCnC logo"/>
+      <img src={logo} alt="AirCnC logo" />
 
       <div className="content">
-        <p>Offer <strong>spots</strong> for programmers and find new <strong>talent</strong> for your company!</p>
+        <p>
+          Offer <strong>spots</strong> for programmers and find new
+          <strong>talent</strong> for your company!
+        </p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">Email</label>
           <input type="email" id="email" placeholder="Email" />
+          <button className="btn" type="submit">
+            Sign in
+          </button>
         </form>
       </div>
     </div>
