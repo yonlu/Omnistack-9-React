ec1f0083f1549986dfb9c68bbf22d01ca21750ab
yonlu <lss.emailbox@gmail.com>
2019-10-15 11:42:15 -0400

    feat: added routing

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
-import React, { useState } from 'react';
-import api from './services/api';
+import React from 'react';
 import './App.css';
 
 import logo from './assets/logo.svg';
 
+import Routes from './routes';
+
 function App() {
-  const [email, setEmail] = useState('');
-
-  function handleSubmit(event) {
-    event.preventDefault();
-
-    // email
-  }
-
   return (
     <div className="container">
       <img src={logo} alt="AirCnC logo" />
 
       <div className="content">
-        <p>
-          Offer <strong>spots</strong> for programmers and find new
-          <strong>talent</strong> for your company!
-        </p>
-
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" placeholder="Email" />
-          <button className="btn" type="submit">
-            Sign in
-          </button>
-        </form>
+        <Routes />
       </div>
     </div>
   );
---
+++ src/pages/Login/index.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import api from '../../services/api';
+
+export default function Login({ history }) {
+  const [email, setEmail] = useState('');
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await api.post('/sessions', {
+      email,
+    });
+
+    const { _id } = response.data;
+
+    localStorage.setItem('user', _id);
+
+    history.push('/dashboard');
+  }
+
+  return (
+    <>
+      <p>
+        Offer <strong>spots</strong> for programmers and find new{' '}
+        <strong>talent</strong> for your company!
+      </p>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          placeholder="Email"
+          value={email}
+          onChange={event => setEmail(event.target.value)}
+        />
+        <button className="btn" type="submit">
+          Sign in
+        </button>
+      </form>
+    </>
+  );
+}
